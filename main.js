a6115914651a1231c1a8a29423e81ba16ca12e3b
vennelakoilada362 <vennelakoilada@gmail.com>
2020-06-30 15:27:40 +0530

    edit commit

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 "use strict"
 
-let add = (a,b) => {
-  let sum = a+b;
-  console.log(sum);
-  return false;
+function getDate(method, url){
+  return new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function(){
+      if(this.status >= 200 && this.status < 300){
+      resolve(xhr.response);
+    }else {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    }
+  };
+  xhr.onerror = function(){
+    reject({
+      status: this.status,
+      statusText: xhr.statusText
+    });
+  };
+  xhr.send();
+});
 }
 
-add(10,2);
+getDate('Get', 'http://jsonplaceholder.typicode.com/todos').then(function(data){
+ let todos = JSON.parse(data)
+ let output = '';
+ for(let todo of todos){
+   output +=`
+   <li>
+     <h3>${todo.title}</h3>
+     <p>Completed: ${todo.completed}</p>
+   </li>
+   `;
+ }
+ document.getElementById('template').innerHTML = output;
+}).catch(function(err){
+  console.log(err);
+});
