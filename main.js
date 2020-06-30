9bf254486f27e16453e679bedb8a92dd500a7658
vennelakoilada362 <vennelakoilada@gmail.com>
2020-06-30 14:50:41 +0530

    edit commit

--- main.js
+++ main.js
@@ -1,38 +1,9 @@
 "use strict"
 
-
-let carWeakMap = new WeakMap();
-
-
-let key1 = {
-  id:1
+let add = (a,b) => {
+  let sum = a+b;
+  console.log(sum);
+  return false;
 }
 
-
-let car1 = {
-  make: 'Honda',
-  model: 'Civic'
-}
-
-
-let key2 = {
-  id:2
-}
-
-
-let car2 = {
-  make: 'Toyota',
-  model: 'Camry'
-}
-
-
-carWeakMap.set(key1, car1);
-
-carWeakMap.set(key2, car2);
-
-
-
-carWeakMap.delete(key1);
-
-
-console.log(carWeakMap);
+add(10,2);
